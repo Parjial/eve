f8e280cbe0204ca72201a7c28dad379614cd9fd7
Parjial <71310577+Parjial@users.noreply.github.com>
2023-04-18 20:46:15 +0530

    Add files via upload

---
+++ eve/js/script.js
@@ -0,0 +1,15 @@
+/* =============================== Typing Animation =================================== */
+var typed = new Typed(".typing", {
+    strings: [
+      "",
+      "We believe in the power of the beautiful game to inspire, unite, and bring joy to people around the world.",
+      "We welcome diversity, celebrate differences, and value the unique contributions of each and every member of our team and community.",
+      "We embodies the spirit of competition, sportsmanship, and fair play, and always strives to leave a positive impact on the communities we serve.",
+      "We stand for more than just results - we stand for passion, dedication, and the pursuit of a shared dream.",
+      "We know that greatness is not achieved overnight, but through hard work, perseverance, and an unwavering commitment to our goals.",
+      "We are driven by a love for the game, a passion for competition, and a deep respect for our opponents and supporters.",
+      "We believe that football has the power to change lives, to bring people together, and to inspire greatness in all who play or watch it."
+    ],
+    
+    loop: true,
+  });
